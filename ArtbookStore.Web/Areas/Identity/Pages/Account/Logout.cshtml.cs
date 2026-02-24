076557fee537eefd125baffe97a2303bef0b2ba9
SaraMsoso <Sara.soder44@gmail.com>
2026-02-24 14:28:07 +0100

    Updated the logout with harden flow using POST-only and role-based redirects and added comments

--- ArtbookStore.Web/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ ArtbookStore.Web/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -1,43 +1,50 @@
-// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-#nullable disable
-
-using System;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
 using ArtbookStore.Web.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
 
 namespace ArtbookStore.Web.Areas.Identity.Pages.Account
 {
+    // AllowAnonymous means even non-logged-in users can reach this page.
+    // We still control what happens in OnGet and OnPost.
+    [AllowAnonymous]
     public class LogoutModel : PageModel
     {
+        // Handles logout of both Admin and Customer. After logout, Admins are redirected to the Login page and Customers to the Home page. SignInManager is responsible for signing out (clearing auth cookie, etc.)
         private readonly SignInManager<ApplicationUser> _signInManager;
-        private readonly ILogger<LogoutModel> _logger;
 
-        public LogoutModel(SignInManager<ApplicationUser> signInManager, ILogger<LogoutModel> logger)
+        public LogoutModel(SignInManager<ApplicationUser> signInManager)
         {
             _signInManager = signInManager;
-            _logger = logger;
         }
 
-        public async Task<IActionResult> OnPost(string returnUrl = null)
+        // GET: /Identity/Account/Logout.
+        // This method is called when the user navigates to the logout page. We don't want to log out immediately on GET  so we just redirect them back to the Home page. The actual logout happens in the POST method when they submit the logout form.
+        public IActionResult OnGet()
         {
+            return RedirectToAction("Index", "Home");
+        }
+
+        // POST: Perform the actual logout
+        // This method is called when the logout form is submitted, determine what role the user has, then log out and redirect based on role.
+        public async Task<IActionResult> OnPost()
+        {
+            // Check if the user is Admin before logout
+            var isAdmin = User.IsInRole("Admin");
+
+            // Log out the user (clear auth cookie, etc.)
             await _signInManager.SignOutAsync();
-            _logger.LogInformation("User logged out.");
-            if (returnUrl != null)
+
+            // Rollbased redirect-logic
+            // Admin redirects to Login page.
+            if (isAdmin)
             {
-                return LocalRedirect(returnUrl);
+                return RedirectToPage("/Account/Login", new { area = "Identity" });
             }
-            else
-            {
-                // This needs to be a redirect so that the browser performs a new
-                // request and the identity for the user gets updated.
-                return RedirectToPage();
-            }
+
+            // Customer redirects to Home page.
+            return RedirectToAction("Index", "Home");
         }
     }
 }
